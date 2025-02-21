890633d88d5df946744c498257b997d8e4b84654
Guolinang <aadyulkeeva@edu.tversu.ru>
2025-02-21 18:41:32 +0300

    A

--- my-rpg-game/main/events/villager.ts
+++ my-rpg-game/main/events/villager.ts
@@ -1,4 +1,5 @@
 import { RpgEvent, EventData, RpgPlayer } from "@rpgjs/server";
+import { createTask } from "../utils/utils";
 
 @EventData({
   name: "EV-1",
@@ -15,8 +16,20 @@ export default class VillagerEvent extends RpgEvent {
     await player.showText("Привет!", {
       talkWith: this,
     });
-    await player.showText("Ты можешь помочь мне?", {
+    await player.showText("Ты можешь помочь мне помыть посуду?", {
       talkWith: this,
+    });
+    await createTask({
+      name: "Помой посуду",
+      s_desc: "Вас попросили помочь помыть посуду",
+      type: "Уборка",
+      stat: "str",
+      difficulty: 1,
+      urgency: new Date(),
+      repeat: "1111111",
+      subtask: "помогите",
+      l_desc: "пожалуйста",
+      important: true,
     });
   }
 }
--- my-rpg-game/main/utils/utils.ts
+++ my-rpg-game/main/utils/utils.ts
@@ -27,3 +27,27 @@ export async function refreshParams(player: RpgPlayer) {
     player.setVariable("maxexp", res.maxexp);
   }
 }
+
+export async function createTask(task) {
+  let response = await fetch("http://localhost:8000/tasks", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json;charset=utf-8",
+      Authorization:
+        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHBpcmVkQXQiOjE3NDI2NTg0NzQsInVzZXJpZCI6IjcifQ.dB2K_ITZjxcvd_QGntsd4Bbfpm9d_WRVgk_EHqHI7j0",
+    },
+    body: JSON.stringify({
+      name: task.name,
+      s_desc: task.s_desc,
+      type: task.type,
+      stat: task.stat,
+      difficulty: task.difficulty,
+      urgency: task.urgency,
+      repeat: task.repeat,
+      subtask: task.subtask,
+      l_desc: task.l_desc,
+      important: task.important,
+      done: false,
+    }),
+  });
+}
